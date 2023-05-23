86502be1ace1004b7302c7b76f2f793131d3818f
Ivan Slukin <provaider010@gmail.com>
2023-05-23 20:11:27 +1000

    fgfg

---
+++ DZ_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.Через строку решать нельзя.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >=100)
+    {
+     while (number > 999)
+     {
+        number = number / 10;
+     }
+        int result = number % 10;
+        System.Console.WriteLine($"Третье число: {result}");
+    }
+    else
+    {
+      System.Console.WriteLine($"третьей цифры нет");
+    }
+ 
+
+ 
